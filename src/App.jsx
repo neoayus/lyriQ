96df9c13e722431a6b00887aff9e80c50f7e74dc
neoayus <ayus43@outlook.com>
2026-02-15 20:33:52 +0530

    chore: figure out how input, type=file works wit images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+// import style
+import { useState } from "react";
+import "./index.css";
+
 function App() {
+  const [image, setImage] = useState();
   return (
     <>
       <p>lyriQ</p>
+      <div
+        className="container"
+        style={{
+          overflow: "hidden", 
+          backgroundImage: `url(${image})`,
+        }}
+      >
+        <p> imma div </p>
+
+        {/* The accept attribute value is a string that defines the file types the file input should accept. */}
+        <input
+          type="file"
+          accept=".png, .jpeg, .jpg"
+          // value={image}
+          onChange={(e) => {
+            const file = e.target.files[0];
+            if (file) {
+              setImage(URL.createObjectURL(file));
+            }
+          }}
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

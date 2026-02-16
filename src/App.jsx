8ded562ecdf291d47bba5871e134fdd626bc43fc
neoayus <ayus43@outlook.com>
2026-02-17 01:26:50 +0530

    refactor: restructure component organization

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,46 @@
 import { useState } from "react";
 import "./index.css";
 
-// import Components 
-import {Logo, Import} from './components/SVGs';
+// import Components
+import { Logo, Import } from "./components/SVGs";
 
 function App() {
-  // const [image, setImage] = useState("/doom.png");
   const [image, setImage] = useState(null);
   const [lyrics, setLyrics] = useState("");
   const [artist, setArtist] = useState("");
   const [song, setSong] = useState("");
-  // Tu aake mera chain cheen le sangeeta, tu kahe to rozz likhu kaveeta :3 
 
   return (
     <>
       <p>lyriQ</p>
+      <LyricsCard image={image} lyrics={lyrics} artist={artist} song={song} />
+      <AddInfo
+        lyrics={lyrics}
+        artist={artist}
+        song={song}
+        setImage={setImage}
+        setLyrics={setLyrics}
+        setArtist={setArtist}
+        setSong={setSong}
+      />
 
       {/* lyrics card */}
-      <div className="card">
+    </>
+  );
+}
 
+export default App;
+
+function LyricsCard({ image, lyrics, artist, song }) {
+  return (
+    <div className="card">
       {/* Conditionally Render this */}
-      {!image ? 
-        <div className="import-screen"> 
+      {!image ? (
+        <div className="import-screen">
           <Import />
           <p>Add an Image</p>
-        </div> 
-          :
+        </div>
+      ) : (
         <div
           className="background"
           style={{
@@ -35,74 +50,73 @@ function App() {
         >
           <p className="lyrics"> {lyrics} </p>
         </div>
-      } 
+      )}
 
-        <div className="card-footer">
-          <p className="song-info">
-            {(artist || song) ? 
-              `${artist}, "${song}"` : ""  
-            }
-          </p>
-          <Logo />
-        </div>
+      <div className="card-footer">
+        <p className="song-info">
+          {artist || song ? `${artist}, "${song}"` : ""}
+        </p>
+        <Logo />
       </div>
-
-      <form className="form">
-        <fieldset>
-            <legend> lyrics card (i) </legend>
-          
-          {/* set image: */}
-            <label>
-              choose image :
-              <input
-                type="file"
-                accept=".png, .jpeg, .jpg"
-                onChange={(e) => {
-                  const file = e.target.files[0];
-                  if (file) {
-                    setImage(URL.createObjectURL(file));
-                  }
-                }}
-            />
-            </label>
-
-          {/* set song: */}
-            <label>
-              Lyrics :
-              <textarea
-                type="text"
-                placeholder="here geos Lyrics..."
-                rows="3"
-                value={lyrics}
-                onChange={(e) => setLyrics(e.target.value)}
-              />
-            </label>
-
-          {/* set song: */}
-            <label>
-              Artist Name :
-              <input
-                type="text"
-                placeholder="Artist"
-                value={artist}
-                onChange={(e) => setArtist(e.target.value)}
-              />
-            </label>
-
-          {/* set song: */}
-            <label>
-              Song Name :
-              <input
-                type="text"
-                placeholder="Song"
-                value={song}
-                onChange={(e) => setSong(e.target.value)}
-              />
-            </label>
-        </fieldset>
-      </form>
-    </>
+    </div>
   );
 }
 
-export default App;
+function AddInfo({ song, artist, lyrics, setSong, setArtist, setLyrics, setImage }) {
+  return (
+    <form className="form">
+      <fieldset>
+        <legend> lyrics card (i) </legend>
+
+        {/* set image: */}
+        <label>
+          choose image :
+          <input
+            type="file"
+            accept=".png, .jpeg, .jpg"
+            onChange={(e) => {
+              const file = e.target.files[0];
+              if (file) {
+                setImage(URL.createObjectURL(file));
+              }
+            }}
+          />
+        </label>
+
+        {/* set song: */}
+        <label>
+          Lyrics :
+          <textarea
+            type="text"
+            placeholder="here geos Lyrics..."
+            rows="3"
+            value={lyrics}
+            onChange={(e) => setLyrics(e.target.value)}
+          />
+        </label>
+
+        {/* set song: */}
+        <label>
+          Artist Name :
+          <input
+            type="text"
+            placeholder="Artist"
+            value={artist}
+            onChange={(e) => setArtist(e.target.value)}
+          />
+        </label>
+
+        {/* set song: */}
+        <label>
+          Song Name :
+          <input
+            type="text"
+            placeholder="Song"
+            value={song}
+            onChange={(e) => setSong(e.target.value)}
+          />
+        </label>
+      </fieldset>
+    </form>
+  );
+}

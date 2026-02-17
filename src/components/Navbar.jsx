fc31c3d12551069337ee121d75dfcd9f559bc6da
neoayus <ayus43@outlook.com>
2026-02-18 00:04:59 +0530

    feat: Menu Sticky Nav in Navigation Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,47 @@
 // import icon
-import { Hamburger } from "./SVGs"
+import { useEffect, useState } from "react"
+import { Hamburger, Cross, Github} from "./SVGs"
 
 export default function Navbar(){
+
+  const [menu, setMenu] = useState(false);
+  
+  useEffect(function(){ // function to lock scroll when menu is open 
+    document.body.style.overflow = menu? "hidden" : "scroll" ; 
+  },[menu])
+
+  function handleNavMenu(){
+    setMenu((menu)=> !menu)
+  }
+
   return(
-    <div className="navbar">
-      <Hamburger />
-      <span className="lyriq">LyriQ</span>
-      <span className="sign-up">SIGN UP </span>
-    </div>
+    <>
+      <div className="navbar">
+        {!menu ? 
+          <>
+            <Hamburger onClick={handleNavMenu}/>
+            <span className="lyriq">LyriQ</span>
+          </>
+          : 
+            <Cross onClick={handleNavMenu}/>
+        }
+        <span className="sign-up">SIGN UP </span>
+      </div>
+
+      {menu && 
+        <div className="nav-menu">
+
+          <ul className="list">
+            <li>Create an Account</li>
+            <li>Saved Cards</li>
+          </ul>
+
+          <div className="sticky-nav" onClick={()=>{window.open("https://github.com/neoayus", "_blank");}}>
+            <Github />
+            <p> neoayus © 2k26</p>
+          </div>
+        </div>
+      }
+    </>
   )
 }
